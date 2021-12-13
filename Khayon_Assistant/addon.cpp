69753bfb801a4052408644574febe4cedb29beaf
bakashigure <bakashigure@hotmail.com>
2021-12-13 23:02:27 +0800

    Update addon.cpp

--- Khayon_Assistant/addon.cpp
+++ Khayon_Assistant/addon.cpp
@@ -1,27 +1,9 @@
 #include <napi.h>
-#include "Khayon.h"
+#include "src/Khayon.h"
 #include <vector>
 
-Napi::Array Method(const Napi::CallbackInfo& info) {
-    Napi::Env env = info.Env();
-
-    Napi::Array ar = Napi::Array::New(env,5);
-
-    std::vector<int> sb = {1,2,3,4,5};
-    for(int i=0;i<sb.size();i++)
-    {
-        ar.Set(i,sb[i]);
-    }
-
-
-    return ar;
-    
+Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+  return Khayon::Init(env, exports);
 }
 
-Napi::Object Init(Napi::Env env, Napi::Object exports) {
-    exports.Set(Napi::String::New(env, "hello"),
-                Napi::Function::New(env, Method));
-    return exports;
-}
-
-NODE_API_MODULE(hello, Init)
+NODE_API_MODULE(addon, InitAll)
