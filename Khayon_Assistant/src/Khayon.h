e405fde8ad8f57c098f7d5fa0f3745aed4c535bf
bakashigure <bakashigure@hotmail.com>
2021-12-14 23:21:00 +0800

    node

--- Khayon_Assistant/src/Khayon.h
+++ Khayon_Assistant/src/Khayon.h
@@ -5,7 +5,6 @@
 #ifndef KHAYON_ASSISTANT_KHAYON_H
 #define KHAYON_ASSISTANT_KHAYON_H
 
-#define GET_ENV Napi::Env env = info.Env();
 
 #include <napi.h>
 #include "Recruitment.h"
@@ -14,16 +13,21 @@
 #include <set>
 #include <map>
 #include <string>
+#include "node.h"
 
-class Khayon : public Napi::ObjectWrap<Khayon>
-{
+#define GET_ENV Napi::Env env = info.Env();
+
+
+class Khayon : public Napi::ObjectWrap<Khayon>{
 public:
     static Napi::Object Init(Napi::Env env, Napi::Object exports);
-    Khayon(const Napi::CallbackInfo &info);
 
-    void init(const Napi::CallbackInfo &info);
-    Napi::Value getOps(const Napi::CallbackInfo &info);  // Napi::Value -> Napi::Array
-    Napi::Value calcTag(const Napi::CallbackInfo &info); //
+    Khayon(const Napi::CallbackInfo& info);
+
+    void init(const Napi::CallbackInfo& info);
+
+    Napi::Value getOps(const Napi::CallbackInfo& info);  // Napi::Value -> Napi::Array
+    Napi::Value calcTag(const Napi::CallbackInfo& info); //
 
 
 private:
--- Khayon_Assistant/src/khayon.cpp
+++ Khayon_Assistant/src/khayon.cpp
@@ -4,19 +4,17 @@
 
 #include "Khayon.h"
 #include <utility>
-
-#define GET_ENV Napi::Env env = info.Env();
+#include <iostream>
+#include <string>
+#include "util.h"
+#include "node.h"
 #define LOG(_CONTENT) _cb->Call({Napi::String::New(env,"log"),Napi::String::New(env,(_CONTENT))});
 
 
-Napi::Object Khayon::Init(Napi::Env env, Napi::Object exports)
-{
-    Napi::Function func =
-        DefineClass(env,
-                    "Khayon",
-                    {InstanceMethod("calcTag", &Khayon::calcTag),
-                     InstanceMethod("getOps", &Khayon::getOps),
-                     InstanceMethod("init", &Khayon::init)});
+Napi::Object Khayon::Init(Napi::Env env, Napi::Object exports) {
+    Napi::Function func = DefineClass(env, "Khayon", {InstanceMethod("calcTag", &Khayon::calcTag),
+                                                       InstanceMethod("getOps",&Khayon::getOps),
+                                                      InstanceMethod("init", &Khayon::init)});
 
     Napi::FunctionReference *constructor = new Napi::FunctionReference();
     *constructor = Napi::Persistent(func);
@@ -26,13 +24,10 @@ Napi::Object Khayon::Init(Napi::Env env, Napi::Object exports)
     return exports;
 }
 
-Khayon::Khayon(const Napi::CallbackInfo &info)
-    : Napi::ObjectWrap<Khayon>(info){
+Khayon::Khayon(const Napi::CallbackInfo& info) : Napi::ObjectWrap<Khayon>(info) {
 
-    Napi::Env env = info.Env();
-
-    if (info.Length() < 1 || !info[0].IsFunction())
-    {
+    GET_ENV
+    if (info.Length() < 1 || !info[0].IsFunction()) {
         Napi::TypeError::New(env, "Log emitter expected").ThrowAsJavaScriptException();
         return;
     }
@@ -40,61 +35,81 @@ Khayon::Khayon(const Napi::CallbackInfo &info)
     Napi::Function cb = info[0].As<Napi::Function>();
     _cb = new Napi::FunctionReference(Napi::Persistent(cb));
 
-    _cb->Call({Napi::String::New(env,"log"),Napi::String::New(env,("Cons"))});
+    _cb->Call({Napi::String::New(env, "log"), Napi::String::New(env, ("Cons"))});
 }
 
 
-void Khayon::init(const Napi::CallbackInfo &info)
-{
+void Khayon::init(const Napi::CallbackInfo& info) {
     GET_ENV
+
     LOG("go to init")
     opi = new kaltsit::OperatorInfo();
-    LOG("1")
-    opi->loadRecruitOperators("../Khayon_Assistant/src/resources/gacha_table.json");
-    LOG("2")
-    opi->loadRecruitOperatorsDetail("../Khayon_Assistant/src/resources/character_table.json");
-    LOG("3")
+    opi->loadTagsEnToZh("E:/Code/c++/Khayon_Arknights/Khayon_Assistant/src/resources/tags_en_to_zh.json");
+    opi->loadRecruitOperators("E:/Code/c++/Khayon_Arknights/Khayon_Assistant/src/resources/gacha_table.json");
+    opi->loadRecruitOperatorsDetail("E:/Code/c++/Khayon_Arknights/Khayon_Assistant/src/resources/character_table.json");
     recruit = new kaltsit::Recruitment(opi->um_tag_ops);
-    LOG("4")
-    /*
-    std::set<std::string> a = {"近战位"};
-    std::set<std::string> b = {"近战位", "狙击干员"};
-    std::set<std::string> c = {"近战位", "狙击干员", "减速"};
-    std::set<std::string> d = {"近战位", "狙击干员", "减速", "削弱"};
 
-    recruit->calcTag(a);
-    recruit->calcTag(b);
-
-    recruit->calcTag(c);
-
-    recruit->calcTag(d);
-    */
-   LOG("init success")
+    LOG("init success")
 }
 
 
-Napi::Value Khayon::calcTag(const Napi::CallbackInfo &info)
-{
+
+Napi::Value Khayon::calcTag(const Napi::CallbackInfo& info) {
     //Napi::Value -> Napi::Array
     GET_ENV
-    std::set<std::string> args;
-    for (int i = 0; i < info.Length(); ++i)
+    if (info.Length() < 1 || info.Length() > 5)
     {
-        args.emplace(info[i].As<Napi::String>());
+        Napi::TypeError::New(env, "Invalid args in calcTag").ThrowAsJavaScriptException();
+        return Napi::String::New(env,"Invalid args in calcTag");
     }
-    recruit->calcTag(args);
 
-    Napi::Array ar = Napi::Array::New(env, 5);
-    std::vector<int> sb = {1, 2, 3, 4, 5};
+    std::set<std::string> args;
+    for (int i = 0; i < info.Length(); ++i) {
+        args.emplace(UtfToGbk(std::string(info[i].As<Napi::String>().Utf8Value()).c_str()));
 
-    ar.Set(uint32_t(0), 114);
-    ar.Set(1, 514);
+    }
+
+    
+    std::cout<<fmt::format("args: {} \n",args);
+    auto res = recruit->calcTag(args);
+
+
+    for(auto& it:res)
+    {
+        auto name_set = recruit->offsetToName(it.first);
+        std::cout<<fmt::format("name_set: {}\n",name_set);
+        std::cout<<fmt::format("ops: {}\n",it.second);
+    }
+
+    auto union_tag = [&](std::set<std::string> st){
+        Napi::Array name_level = Napi::Array::New(env);
+        for(auto& it:st)
+        {
+            name_level.Set(Napi::String::New(env,GbkToUtf(it)),opi->getStar(it));
+        }
+        return name_level;
+    };
+
+   
+    std::cout<<"\n_________res_________:\n";
+    for(auto& it:res)
+    {
+        std::cout<<fmt::format("k:{} v:{}\n",it.first,it.second);
+    }
+   
+
+   
+    Napi::Array ar = Napi::Array::New(env,res.size());
+    for(auto& it:res)
+    {
+        ar.Set(std::to_string(it.first),union_tag(it.second));
+    }
 
     return ar;
 }
 
-Napi::Value Khayon::getOps(const Napi::CallbackInfo &info)
-{
+Napi::Value Khayon::getOps(const Napi::CallbackInfo& info) {
     GET_ENV
-    return Napi::Number::New(env, 0);
+    
+    return Napi::String::New(env,"测试");
 }
